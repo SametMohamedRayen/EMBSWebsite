cd78f5509e05532607359dc2c6563198891c293a
MohamedRayenSamet <rayensamet77@gmail.com>
2022-10-03 00:29:31 +0100

    Beautifying Staff Section + Animations

--- src/components/MiddleTopSection.js
+++ src/components/MiddleTopSection.js
@@ -1,25 +1,127 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StaffElement from "./StaffElement";
+import rubyCrown from "../images/crowns.png";
+import basicCrown from "../images/crown.png";
+import moneyBag from "../images/money-bag.png";
+import glasses from "../images/glasses.png";
+import bookAndQuill from "../images/writing.png";
 
 const MiddleTopSection = () => {
+  const [staffOn, setStaff] = useState(false);
+  const [rubyCrownHover, setRubyCrownHover] = useState(false);
+  const [basicCrownHover, setBasicCrownHover] = useState(false);
+  const [moneyBagHover, setMoneyBagHover] = useState(false);
+  const [glassesHover, setGlassesHover] = useState(false);
+  const [bookAndQuillHover, setBookAndQuillHover] = useState(false);
+
+  const BringStaffOn = () => {
+    if (window.scrollY >= 300) {
+      setStaff(true);
+    }
+  };
+
+  useEffect(() => {
+    BringStaffOn();
+    window.addEventListener("scroll", BringStaffOn);
+  });
+
   return (
-    <div className="middleTopSection">
-      <h1 className="newsHeadline">Meet the Team</h1>
-      <div className="staffGrid">
-        <div className="rowThirds">
-          <StaffElement staffName="Ahlem Marzougi" staffPosition="Chair" />
-          <StaffElement staffName="Ahmed Chtourou" staffPosition="Vice Chair" />
-          <StaffElement staffName="Oumayma Trifii" staffPosition="Treasurer" />
-        </div>
-        <div className="rowHalves">
-          <StaffElement
-            staffName="Mohamed Rayen Samet"
-            staffPosition="Webmaster"
-          />
-          <StaffElement staffName="Mohamed Turki" staffPosition="Secretary" />
+    <>
+      <img
+        src={require("../images/MiddleTopSectionBg.png")}
+        className="bgMiddleTopSection"
+      />
+      <div className="middleTopSection">
+        <h1
+          className={
+            "staffHeadlineShadow" + (staffOn ? "On" : "Off") + " absolute"
+          }
+        >
+          M<span style={{ color: "#0CC5CB" }}>ieee</span>t the Team
+        </h1>
+        <h1 className={"staffHeadline" + (staffOn ? "On" : "Off")}>
+          M<span style={{ color: "#0CC5CB" }}>ieee</span>t the Team
+        </h1>
+        <div className="staffGrid">
+          <div className="rowThirds">
+            <div
+              onMouseEnter={() => setRubyCrownHover(true)}
+              onMouseLeave={() => setRubyCrownHover(false)}
+              className="fit-content"
+            >
+              <StaffElement
+                staffName="Ahlem Marzougi"
+                staffPosition="Chair"
+                emoji={rubyCrown}
+                emojiPosition={
+                  rubyCrownHover ? "crownPositionHover" : "crownPosition"
+                }
+              />
+            </div>
+            <div
+              onMouseEnter={() => setBasicCrownHover(true)}
+              onMouseLeave={() => setBasicCrownHover(false)}
+              className="fit-content"
+            >
+              <StaffElement
+                staffName="Ahmed Chtourou"
+                staffPosition="Vice Chair"
+                emoji={basicCrown}
+                emojiPosition={
+                  basicCrownHover ? "crownPositionHover" : "crownPosition"
+                }
+              />
+            </div>
+            <div
+              onMouseEnter={() => setMoneyBagHover(true)}
+              onMouseLeave={() => setMoneyBagHover(false)}
+              className="fit-content"
+            >
+              <StaffElement
+                staffName="Oumayma Trifii"
+                staffPosition="Treasurer"
+                emoji={moneyBag}
+                emojiPosition={
+                  moneyBagHover ? "moneyBagPositionHover" : "moneyBagPosition"
+                }
+              />
+            </div>
+          </div>
+          <div className="rowHalves">
+            <div
+              onMouseEnter={() => setGlassesHover(true)}
+              onMouseLeave={() => setGlassesHover(false)}
+              className="fit-content"
+            >
+              <StaffElement
+                staffName="Mohamed Rayen Samet"
+                staffPosition="Webmaster"
+                emoji={glasses}
+                emojiPosition={
+                  glassesHover ? "glassesPositionHover" : "glassesPosition"
+                }
+              />
+            </div>
+            <div
+              onMouseEnter={() => setBookAndQuillHover(true)}
+              onMouseLeave={() => setBookAndQuillHover(false)}
+              className="fit-content"
+            >
+              <StaffElement
+                staffName="Mohamed Turki"
+                staffPosition="Secretary"
+                emoji={bookAndQuill}
+                emojiPosition={
+                  bookAndQuillHover
+                    ? "bookAndQuillPositionHover"
+                    : "bookAndQuillPosition"
+                }
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/StaffElement.js
+++ src/components/StaffElement.js
@@ -2,12 +2,13 @@ import React from "react";
 import { ReactComponent as StaffFrame } from "../SVG/StaffFrame.svg";
 
 const StaffElement = (props) => {
-  const { staffName, staffPosition } = props;
+  const { staffName, staffPosition, emoji, emojiPosition } = props;
   return (
     <div className="staffElement">
       <StaffFrame className="staffSvg" />
       <h2 className="staffName">{staffName}</h2>
       <h3 className="staffPosition">{staffPosition}</h3>
+      <img src={emoji} className={emojiPosition} />
     </div>
   );
 };
