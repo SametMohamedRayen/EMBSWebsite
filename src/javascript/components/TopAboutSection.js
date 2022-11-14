e9c5c266159ce19365586e41d3ee9b08b19ca41c
MohamedRayenSamet <rayensamet77@gmail.com>
2022-11-14 16:27:13 +0100

    First version of the about view

--- src/javascript/components/TopAboutSection.js
+++ src/javascript/components/TopAboutSection.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { ReactComponent as AboutTitleFrame } from "../../assets/svg/AboutTitleFrame.svg";
+import { ReactComponent as DNAFull } from "../../assets/svg/DNAFull.svg";
+import { ReactComponent as AboutTitleFrame } from "./../../assets/svg/AboutTitleFrame.svg";
 
 const TopAboutSection = () => {
   return (
     <div className="relative height-50 margin-btm-15">
       <div className="absolute width-full">
-        <div className="aboutEmbsTitle width-full relative">
-          <AboutTitleFrame className="aboutTitleFrame" />
-          <div className="whoAreEmbs">
-            <h1 className="font-indigo-regular font-color-blue font-size-250 letter-space-50 margin-0">
-              Who <br></br>Are
-            </h1>
+        <DNAFull className="DNAFullAbout" />
+        <div className="bg-radial-gradient-white">
+          <div className="aboutEmbsTitle width-full relative">
+            <div className="whoAreEmbs">
+              <h1 className="font-indigo-regular font-color-blue font-size-4 letter-space-50 margin-0">
+                Who Are
+              </h1>
+            </div>
           </div>
+          <img
+            src={require("../../assets/images/NamelessEMBSLogo.png")}
+            className="aboutEmbsLogo"
+            alt="EMB"
+          />
         </div>
-        <img
-          src={require("../../assets/images/NamelessEMBSLogo.png")}
-          className="aboutEmbsLogo"
-          alt="EMB"
-        />
         <div className="centerContent">
-          <p className="centerText width-half font-albori letter-space-125 font-size-140 font-color-darkBlue">
+          <AboutTitleFrame className="absolute width-full margin-top-neg-10" />
+          <p className="centerText width-half font-albori letter-space-125 font-size-140 font-color-darkBlue margin-top-4">
             IEEE Engineering in Medicine and Biology Society (EMBS) is the
             world’s largest international society of biomedical engineers. The
             organization’s 12,000 members reside in some 97 countries around the
