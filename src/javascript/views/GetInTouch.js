f2afa732bb5200723ea66236a3ab7e69cc2b8356
MohamedRayenSamet <rayensamet77@gmail.com>
2022-11-18 12:15:41 +0100

    Added Bg

--- src/javascript/views/GetInTouch.js
+++ src/javascript/views/GetInTouch.js
@@ -4,6 +4,11 @@ import ContactUsSection from "../components/ContactUsSection";
 const GetInTouch = () => {
   return (
     <div className="sections">
+      <img
+        src={require("../../assets/images/ContactUsBg.png")}
+        className="bgContactUs"
+        alt="background"
+      />
       <ContactUsSection />
     </div>
   );
